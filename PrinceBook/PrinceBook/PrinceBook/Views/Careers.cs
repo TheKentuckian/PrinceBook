8a2c2284725bf136baab34bece2967ee2e22d57f
Mohamed Idries <moidriez@gmail.com>
2015-02-01 03:16:10 +0000

    master - added missing project

---
+++ PrinceBook/PrinceBook/PrinceBook/Views/Careers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PrinceBook.Views
+{
+    public class Careers : ContentPage
+    {
+        public Careers()
+        {
+            var layout = new StackLayout { Padding = 10 };
+            var label = new Label
+            {
+                Text = "Welcome to the Career Page.",
+                Font = Font.BoldSystemFontOfSize(NamedSize.Large),
+                TextColor = Color.White,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                XAlign = TextAlignment.Center, // Center the text in the blue box.
+                YAlign = TextAlignment.Center, // Center the text in the blue box.
+            };
+            layout.Children.Add(label);
+
+            Content = new ScrollView { Content = layout };
+
+        }
+    }
+}
